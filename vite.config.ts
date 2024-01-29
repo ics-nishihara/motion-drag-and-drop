cfeb65c50a27f403dcd48ab42a04e23016b48572
ics-nishihara <nishihara@ics-web.jp>
2024-01-29 22:22:02 +0900

    パスの修正

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
+  base: mode === "production" ? "/motion-drag-and-drop/" : "/",
   build: {
     outDir: "./docs",
   },
-});
+}));
